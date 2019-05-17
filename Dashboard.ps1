4699c68a38ed454881bbf6a1ad89c6ac086436fe
Daniel Morritt <daniel@morritt.tv>
2019-05-17 22:05:53 +0100

    Removed commented out code

--- Dashboard.ps1
+++ Dashboard.ps1
@@ -1,7 +1,4 @@
-﻿#Set-Location D:\Dev\BitBoard
-#Import-Module $PSScriptRoot\modules\Bitcoin-Module.psm1
-
-$HomePage = . (Join-Path $PSScriptRoot "pages\Home.ps1")
+﻿$HomePage = . (Join-Path $PSScriptRoot "pages\Home.ps1")
 $PricesPage = . (Join-Path $PSScriptRoot "pages\Prices.ps1")
 $RedditPage = . (Join-Path $PSScriptRoot "pages\Reddit.ps1")
 $BlockPage = . (Join-Path $PSScriptRoot "pages\Block.ps1")
